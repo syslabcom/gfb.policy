ace2fe973a35e533474590c0dbc3f505e85c0b96
Alexander Pilz <pilz@syslab.com>
2007-12-03 13:35:42 +0000

    added final imports

---
+++ gfb/policy/tests/test_setup.py
@@ -0,0 +1,22 @@
+import unittest
+from gfb.policy.tests.base import GFBPolicyTestCase
+
+from Products.CMFCore.utils import getToolByName
+
+class TestSetup(GFBPolicyTestCase):
+    
+    def afterSetUp(self):
+        self.workflow = getToolByName(self.portal, 'portal_workflow')
+        self.acl_users = getToolByName(self.portal, 'acl_users')
+        self.types = getToolByName(self.portal, 'portal_types')
+    
+    def test_portal_title(self):
+        self.assertEquals("GFB Portal", self.portal.getProperty('title'))
+        
+    def test_portal_description(self):
+        self.assertEquals("Welcome to the GFB portal", self.portal.getProperty('description'))
+        
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestSetup))
+    return suite
