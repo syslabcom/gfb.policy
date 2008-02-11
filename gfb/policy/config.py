8987f85553e1c765444bd09e102214c97388c582
Wolfgang Thomas <thomas@syslab.com>
2008-02-11 14:03:01 +0000

    bugfix + filtering languages

--- gfb/policy/config.py
+++ gfb/policy/config.py
@@ -1,3 +1,5 @@
 # Configuration for the policy
 PROVIDER_ROLE = "Provider"
 INSTALL_LDAP = 1
+
+AVAILABLE_LANGUAGES_RAL = ('de', 'en', 'fr', 'it', 'da')
--- gfb/policy/patch_RAL.py
+++ gfb/policy/patch_RAL.py
@@ -1,4 +1,7 @@
 from Products.CMFCore.utils import getToolByName
+from config import AVAILABLE_LANGUAGES_RAL
+from zope.i18n import translate
+from Products.Archetypes.utils import DisplayList
 
 from Products.RiskAssessmentLink.content.RiskAssessmentLink import RiskAssessmentLink, RiskAssessmentLink_schema as schema
 schema['provider'].widget.visible['edit'] = 'invisible'
@@ -23,5 +26,21 @@ def at_post_edit_script(self):
     if provider:
         self.setProvider(provider.UID())
 
+
+
+def getFilteredLanguages(self):
+    """ return the languages filtered by the abbreviations given in the config file """
+    plt = getToolByName(self, 'portal_languages')
+    langs = plt.listAvailableLanguages()
+    L = []
+    for l in langs:
+        if l[0] in AVAILABLE_LANGUAGES_RAL:
+            L.append((l[0], translate(l[1]) ))
+    L.sort()
+    return DisplayList(L)
+
 RiskAssessmentLink.at_post_edit_script = at_post_edit_script
-RiskAssessmentLink.at_post_create_script = RiskAssessmentLink
+RiskAssessmentLink.at_post_create_script = at_post_edit_script
+RiskAssessmentLink.getFilteredLanguages = getFilteredLanguages
+
+
