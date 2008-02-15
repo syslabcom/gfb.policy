5a93c1a0d74fbdbd402ad09a60dadf78a63f0dda
Wolfgang Thomas <thomas@syslab.com>
2008-02-15 17:58:49 +0000

    the good old logit...

--- gfb/policy/__init__.py
+++ gfb/policy/__init__.py
@@ -1,4 +1,7 @@
 import patch_RAL
 
+from AccessControl import ModuleSecurityInfo
+ModuleSecurityInfo('gfb.policy.utils').declarePublic('logit')
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
