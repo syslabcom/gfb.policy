6fb4b14b3c753c9ef4787f549e862efc647908c5
Wolfgang Thomas <thomas@syslab.com>
2008-03-03 13:34:40 +0000

    Permission "Change local roles" removed from user folder

--- gfb/policy/Extensions/notifyMemberAreaCreated.py
+++ gfb/policy/Extensions/notifyMemberAreaCreated.py
@@ -9,3 +9,5 @@ def notifyMemberAreaCreated(self, **args):
             hf._setProperty('layout', '@@workingarea', 'string')
         else:
             hf._updateProperty('layout', '@@workingarea')
+
+    hf.manage_permission(permission_to_manage="Change local roles", roles=['Manager'], acquire=0)
