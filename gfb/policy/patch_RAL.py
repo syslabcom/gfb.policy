f45307e8aa7a961212b4c964f070766e9ab18593
Wolfgang Thomas <thomas@syslab.com>
2008-05-06 17:02:09 +0000

    parts of the patch have been moved to the product

--- gfb/policy/patch_RAL.py
+++ gfb/policy/patch_RAL.py
@@ -2,12 +2,7 @@ from Products.CMFCore.utils import getToolByName
 from config import AVAILABLE_LANGUAGES_RAL
 from zope.i18n import translate
 from Products.Archetypes.utils import DisplayList
-
-from Products.RiskAssessmentLink.content.RiskAssessmentLink import RiskAssessmentLink, RiskAssessmentLink_schema as schema
-schema['provider'].widget.visible['edit'] = 'invisible'
-schema['provider'].widget.visible['view'] = 'invisible'
-schema['remoteProvider'].widget.visible['edit'] = 'invisible'
-schema['remoteProvider'].widget.visible['view'] = 'invisible'
+from Products.RiskAssessmentLink.content.RiskAssessmentLink import RiskAssessmentLink
 
 
 from Products.RemoteProvider.content.Provider import Provider, Provider_schema
@@ -23,44 +18,6 @@ for name in unwantedFields:
 # make providerCategory required
 Provider_schema['providerCategory'].required = True
 
-from AccessControl import ClassSecurityInfo
-security = ClassSecurityInfo()
-
-
-security.declarePublic('at_post_edit_script')
-def at_post_edit_script(self):
-    """
-    """
-
-    def getMyProvider(self):
-        pm = getToolByName(self, 'portal_membership')
-        hf = pm.getHomeFolder()
-        try:
-            name = pm.getAuthenticatedMember().getUserId()
-        except:
-            name = pm.getAuthenticatedMember().getUserName()
-        f = pm.getMembersFolder()
-        path = "/".join( f.getPhysicalPath() ) + '/' + name
-        res = self.portal_catalog(portal_type='Provider', path=path)
-        return len(res) > 0 and res[0].getObject() or None
-
-    doReindex = False
-    riskfactors = self.getRiskfactors()
-    occupation = self.getOccupation()
-    nace =self.getField('nace').getAccessor(self)()
-    if '0' in riskfactors and '0' in occupation and '0' in nace:
-        self.setCategoryIndependent(True)
-        doReindex = True
-
-    provider = getMyProvider(self)
-    if provider:
-        self.setRemoteProvider(provider.UID())
-        doReindex = True
-
-    if doReindex:
-        self.reindexObject()
-
-
 
 def getFilteredLanguages(self):
     """ return the languages filtered by the abbreviations given in the config file """
@@ -73,8 +30,6 @@ def getFilteredLanguages(self):
     L.sort()
     return DisplayList(L)
 
-RiskAssessmentLink.at_post_edit_script = at_post_edit_script
-RiskAssessmentLink.at_post_create_script = at_post_edit_script
 RiskAssessmentLink.getFilteredLanguages = getFilteredLanguages
 
 
