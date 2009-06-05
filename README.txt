6d15b5b7bb110a31c52046c12613f627131e944a
Alexander Pilz <pilz@syslab.com>
2009-06-05 13:53:55 +0000

    update for eggification

--- README.txt
+++ README.txt
@@ -1,7 +1,16 @@
-gfb.policy Package Readme
-=========================
+.. contents::
 
-Overview
---------
+.. Note!
+   -----
+   
+   - code repository
+   - bug tracker
+   - questions/comments feedback mail
 
-GFB Policy
+
+- Code repository: https://svn.syslab.com/svn/syslabcom/gfb
+- Questions and comments to info (at) syslab (dot) com
+- Report bugs at http://products.syslab.com/products/gfb.policy/issues
+
+
+
--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the gfb.policy package
+"""
+import os
 from setuptools import setup, find_packages
 
-version = '0.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('gfb', 'policy', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' 
+    )
+    
+tests_require=['zope.testing']
 
 setup(name='gfb.policy',
       version=version,
       description="GFB Policy",
-      long_description="""\
-""",
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -26,9 +55,16 @@ setup(name='gfb.policy',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'gfb.theme',
+          
           # -*- Extra requirements: -*-
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'gfb.policy.tests.test_docs.test_suite',
       entry_points="""
-      # -*- Entry points: -*-
+      # -*- entry_points -*- 
       """,
-      )
+      paster_plugins = ["ZopeSkel"],
+      )      
+      
