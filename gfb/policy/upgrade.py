fe911c3ee301e8cbee40ea56b57aa163d2685a42
Wolfgang Thomas <thomas@syslab.com>
2011-07-21 16:03:07 +0000

    added upgrade step for the language tool

--- gfb/policy/upgrade.py
+++ gfb/policy/upgrade.py
@@ -1,6 +1,7 @@
 from Products.CMFCore.utils import getToolByName
 import logging
 
+
 def fixGenericSetup(self):
     logger = logging.getLogger("fixGenericSetup")
     logger.info("Fixing Generic Setup")
@@ -17,4 +18,23 @@ def fixGenericSetup(self):
     ids = [x['id'] for x in invalid_steps]
     gs.manage_deleteExportSteps(ids)
     logger.info("Removed %d invalid export steps" % len(invalid_steps))
-    
+
+
+def configureLanguageTool(self):
+    plt = getToolByName(self, 'portal_languages')
+    settings = {'use_path_negotiation': 0,
+        'use_cookie_negotiation': 1,
+        'display_flags': 1,
+        'force_language_urls': 0,
+        'start_neutral': 0,
+        'set_cookie_everywhere': 0,
+        'use_subdomain_negotiation': 0,
+        'use_content_negotiation': 0,
+        'use_request_negotiation': 1,
+        'authenticated_users_only': 0,
+        'use_combined_language_codes': 0,
+        'allow_content_language_fallback': 1,
+        'use_cctld_negotiation': 0}
+    for k, v in settings.items():
+        setattr(plt, k, v)
+       
