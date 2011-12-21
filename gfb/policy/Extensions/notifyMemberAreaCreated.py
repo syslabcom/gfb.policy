d89d1481ed328289986764792b3ac6045f491c1a
Wolfgang Thomas <thomas@syslab.com>
2011-12-21 18:11:44 +0000

    set the default editor to CKeditor for all new users

--- gfb/policy/Extensions/notifyMemberAreaCreated.py
+++ gfb/policy/Extensions/notifyMemberAreaCreated.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
+
+# Note: this ExternalMethod is used in the Members folder
+# It is called via a hook if a new member folder is created
+# The script MUST be named notifyMemberAreaCreated
 
 def notifyMemberAreaCreated(self, **args):
     pm = getToolByName(self, 'portal_membership')
@@ -10,4 +15,6 @@ def notifyMemberAreaCreated(self, **args):
         else:
             hf._updateProperty('layout', '@@workingarea')
 
+    member = pm.getAuthenticatedMember()
+    member.setMemberProperties({'wysiwyg_editor': 'CKeditor'})
     hf.manage_permission(permission_to_manage="Change local roles", roles=['Manager'], acquire=0)
