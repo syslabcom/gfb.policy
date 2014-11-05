b51c76ced7f59e515c60cb2bc9cc56c6630da0d1
Wolfgang Thomas <thomas@syslab.com>
2014-11-05 16:47:09 +0100

    marker interface for certain news listings that should use their folder's title as page title

--- gfb/policy/interfaces.py
+++ gfb/policy/interfaces.py
@@ -1,6 +1,11 @@
 from zope.interface import Interface
 
+
 class IVocabularyUtility(Interface):
     """A utility that returns a vocabulary dict"""
     def getVocabularyDict():
         """return the dict"""
+
+
+class INewsListing(Interface):
+    pass
