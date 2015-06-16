cb5fe901f01d2bb2d081253f45ce9efbeb8af47c
Wolfgang Thomas <thomas@syslab.com>
2015-06-16 18:36:10 +0200

    extend user data schema with field is_author_schema #11852

--- gfb/policy/Extensions/notifyMemberAreaCreated.py
+++ gfb/policy/Extensions/notifyMemberAreaCreated.py
@@ -5,16 +5,21 @@ from Products.CMFCore.utils import getToolByName
 # It is called via a hook if a new member folder is created
 # The script MUST be named notifyMemberAreaCreated
 
+
 def notifyMemberAreaCreated(self, **args):
     pm = getToolByName(self, 'portal_membership')
-    hf = pm.getHomeFolder()
-
-    if hf is not None:
-        if not hf.hasProperty('layout'):
-            hf._setProperty('layout', '@@workingarea', 'string')
-        else:
-            hf._updateProperty('layout', '@@workingarea')
-
     member = pm.getAuthenticatedMember()
+
+    hf = pm.getHomeFolder()
+    if not member.getProperty('is_expert_author', False):
+        if hf is not None:
+            if not hf.hasProperty('layout'):
+                hf._setProperty('layout', '@@workingarea', 'string')
+            else:
+                hf._updateProperty('layout', '@@workingarea')
+
     member.setMemberProperties({'wysiwyg_editor': 'CKeditor'})
-    hf.manage_permission(permission_to_manage="Change local roles", roles=['Manager'], acquire=0)
+    if hf is not None:
+        hf.manage_permission(
+            permission_to_manage="Change local roles",
+            roles=['Manager'], acquire=0)
