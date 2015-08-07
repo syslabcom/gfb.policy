52a06bccc2bd9d1ccb3f3ad15fc2de356bd37156
Cillian de Róiste <deroiste@syslab.com>
2015-08-07 10:39:58 +0200

    Remove the linkintegrity_unicode_patch to upgrade p.a.linkintegrity

--- gfb/policy/patches/__init__.py
+++ gfb/policy/patches/__init__.py
@@ -2,7 +2,6 @@
 #import proxyindex_patch
 import PHC_Vocabulay_patch
 import newsitem_patch
-import linkintegrity_unicode_patch
 import patch_copy_language_independent_fields
 import cmfeditions_patch
 import catalog_patches
