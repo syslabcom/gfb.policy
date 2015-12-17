7b77a9ccfe93162e02d15fc109190103c99aead2
Wolfgang Thomas <thomas@syslab.com>
2015-12-17 19:12:52 +0100

    actually, we don't want to prettify the HTML, but just render it

--- gfb/policy/patches/patch_atdocument.py
+++ gfb/policy/patches/patch_atdocument.py
@@ -1,15 +1,15 @@
 from Products.ATContentTypes.content.document import ATDocument
 from Products.CMFPlone.utils import safe_unicode
-from gfb.policy.BeautifulSoup import BeautifulStoneSoup
+from gfb.policy.BeautifulSoup import BeautifulSoup
 
 orig_processForm = ATDocument.processForm
 
 
 def processForm(self, data=1, metadata=0, REQUEST=None, values=None):
     REQUEST = REQUEST or self.REQUEST
-    bs = BeautifulStoneSoup(
+    bs = BeautifulSoup(
         safe_unicode(REQUEST.get('text', '')), convertEntities='html')
-    REQUEST.form['text'] = bs.prettify('utf-8').replace('\xc2\xa0', '&nbsp;')
+    REQUEST.form['text'] = bs.renderContents('utf-8').replace('\xc2\xa0', '&nbsp;')
     orig_processForm(self, data, metadata, REQUEST, values)
 
 ATDocument.processForm = processForm
